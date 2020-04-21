20c8dc4c12c3643aa7d45f05444f7f89f46a3063
Matthias <matthias.cartel@gmail.com>
2020-04-22 00:57:29 +0200

    Home Component

--- components/Home.js
+++ components/Home.js
@@ -1,54 +1,56 @@
-import React from 'react'
-import { View, Text, Animated, StyleSheet } from 'react-native'
+import React from 'react';
+import {
+  View, Text, Animated, StyleSheet,
+} from 'react-native';
 
 export default class Home extends React.Component {
-    constructor(){
-        super();
-        //this.fadeAnime =  new Animated.Value(0);
-    }
+  constructor() {
+    super();
+    // this.fadeAnime =  new Animated.Value(0);
+  }
 
-    // animatedText = () => {
-    //     Animated.timing(this.fadeAnime, {
-    //         toValue: 100,
-    //         duration: 2000
-    //     }).start()
-    //     Animated.timing(this.fadeAnime, {
-    //         toValue: 0,
-    //         duration: 2000
-    //     }).start()
-    // }
-    render () {
-        return (
-            <View style={styles.container}>
-               {/*Animate first time here opacity = 1*/}
-               {/*Animate second time here opacity = 0 */}
-                <Animated.View
-                style={styles.fadingContainer}
-                >
-                    <Text style={styles.titleContainer}>
-                        Bienvenue sur Cooloc !
-                    </Text>
-                </Animated.View>
-            </View>
-        )
-    }
+  // animatedText = () => {
+  //     Animated.timing(this.fadeAnime, {
+  //         toValue: 100,
+  //         duration: 2000
+  //     }).start()
+  //     Animated.timing(this.fadeAnime, {
+  //         toValue: 0,
+  //         duration: 2000
+  //     }).start()
+  // }
 
+  render() {
+    return (
+      <View style={styles.container}>
+        {/* Animate first time here opacity = 1 */}
+        {/* Animate second time here opacity = 0 */}
+        <Animated.View
+          style={styles.fadingContainer}
+        >
+          <Text style={styles.titleContainer}>
+            Bienvenue sur Cooloc !
+          </Text>
+        </Animated.View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    titleContainer:{
-        fontSize: 20,
-        fontWeight: 'bold',
-        fontFamily: 'Montserrat',
-        color: '#191970',
-    },
-    container:{
-        textAlign: 'center'
-    }, 
-    fadingContainer: {
-        paddingVertical: 8,
-        paddingHorizontal: 16,
-        backgroundColor: "fa8072",
-        borderRadius: '20px'
-      },  
-})
+  titleContainer: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    fontFamily: 'Montserrat',
+    color: '#191970',
+  },
+  container: {
+    textAlign: 'center',
+  },
+  fadingContainer: {
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    backgroundColor: 'fa8072',
+    borderRadius: '20px',
+  },
+});
